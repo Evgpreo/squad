218f3ab05546e33bf959c5d3e66cd8c21093b2f0
Evgenia <evgpreo@gmail.com>
2023-03-12 13:56:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const arr1 = [7, 4, 5, 6, 8]; //подготовили массив
+
 function squad(item) {
     return item**2;
 }
 const arr2 = arr1.map(squad);
 console.log(arr2)
+
+
+//переделываем в анонимную функцию
+
+// const arr2 = arr1.map(item => item**2);
+// console.log(arr2)
+// showArr(arr2, out3);
